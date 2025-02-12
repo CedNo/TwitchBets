82a2b354d6d9f916c995a989e35b243a65a5958b
Cedric <cedric.noel.2@ulaval.ca>
2025-02-12 17:55:29 -0500

    Project Setup

---
+++ src/main/java/com/api/twitchbets/TwitchbetsApplication.java
@@ -0,0 +1,12 @@
+package com.api.twitchbets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchbetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchbetsApplication.class, args);
+	}
+}
