4438f3aa709c3a11ce17876f144256bf3d975238
Cedric <cedric.noel.2@ulaval.ca>
2025-03-07 12:36:53 -0500

    Added UUID id and User user attibutes to Bet.java

--- src/main/java/com/api/twitchbets/domain/bet/Bet.java
+++ src/main/java/com/api/twitchbets/domain/bet/Bet.java
@@ -1,10 +1,18 @@
 package com.api.twitchbets.domain.bet;
 
+import java.util.UUID;
+
+import com.api.twitchbets.domain.user.User;
+
 public class Bet {
 
-    private float amount;
+    private final UUID id;
+    private final User user;
+    private final float amount;
 
-    public Bet(float amount) {
+    public Bet(UUID id, User user, float amount) {
+        this.id = id;
+        this.user = user;
         this.amount = amount;
     }
 
