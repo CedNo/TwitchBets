f1ae1129b7597d9910e3919de0a95a910cb1f640
Cedric <cedric.noel.2@ulaval.ca>
2025-04-17 19:56:42 -0400

    Fixed endTime formating for AddBetQuestionRequest

--- src/main/java/com/api/twitchbets/interfaces/dto/requests/AddBetQuestionRequest.java
+++ src/main/java/com/api/twitchbets/interfaces/dto/requests/AddBetQuestionRequest.java
@@ -3,12 +3,19 @@ package com.api.twitchbets.interfaces.dto.requests;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record AddBetQuestionRequest(
-    @JsonProperty String question,
-    @JsonProperty List<String> options,
-    @JsonProperty LocalDateTime endTime
+    @JsonProperty
+    String question,
+
+    @JsonProperty
+    List<String> options,
+
+    @JsonProperty
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime endTime
 ) {
 
 }
