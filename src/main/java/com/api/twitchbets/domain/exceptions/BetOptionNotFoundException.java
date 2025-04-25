be91db2ffa3561e2f5bc7c4f26e7d6be053908f1
Cédric Noël <70913418+CedNo@users.noreply.github.com>
2025-04-25 10:40:42 -0400

    Generalized NotFoundException (#11)

--- src/main/java/com/api/twitchbets/domain/exceptions/BetOptionNotFoundException.java
+++ src/main/java/com/api/twitchbets/domain/exceptions/BetOptionNotFoundException.java
@@ -2,7 +2,7 @@ package com.api.twitchbets.domain.exceptions;
 
 import java.util.UUID;
 
-public class BetOptionNotFoundException extends GenericException {
+public class BetOptionNotFoundException extends NotFoundException {
 
     public BetOptionNotFoundException(UUID id) {
         super("BET_OPTION_NOT_FOUND", String.format("Bet Option %s not found", id));
--- src/main/java/com/api/twitchbets/domain/exceptions/UserNotFoundException.java
+++ src/main/java/com/api/twitchbets/domain/exceptions/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.api.twitchbets.domain.exceptions;
 
-public class UserNotFoundException extends GenericException {
+public class UserNotFoundException extends NotFoundException {
 
     public UserNotFoundException(String username) {
         super("USER_NOT_FOUND", String.format("User %s not found", username));
