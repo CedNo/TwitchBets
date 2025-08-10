05c8f7c74b5a8efed42dfa16b23b694767ceec3c
Cedric <cedric.noel.2@ulaval.ca>
2025-08-10 16:39:45 -0400

    Added app ip to allowed cors origin

--- src/main/java/com/api/twitchbets/Configuration.java
+++ src/main/java/com/api/twitchbets/Configuration.java
@@ -3,9 +3,22 @@ package com.api.twitchbets;
 import java.time.Clock;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5000");
+            }
+        };
+    }
 
     @Bean
     public Clock clock() {
