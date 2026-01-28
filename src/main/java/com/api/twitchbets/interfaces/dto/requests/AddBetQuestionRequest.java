ab5109a5b82e9aad117ec237f2edcf80c962785f
Cédric Noël <70913418+CedNo@users.noreply.github.com>
2026-01-28 16:40:09 -0500

    Added validations to AddBetQuestionRequest (#18)

--- src/main/java/com/api/twitchbets/interfaces/dto/requests/AddBetQuestionRequest.java
+++ src/main/java/com/api/twitchbets/interfaces/dto/requests/AddBetQuestionRequest.java
@@ -6,21 +6,25 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record AddBetQuestionRequest(
-    @NotBlank
+    @NotBlank(message = "Question cannot be blank")
     @JsonProperty
     String question,
 
     @NotNull
+    @Size(min=2, message = "At least two options are required")
     @JsonProperty
     List<String> options,
 
     @NotNull
     @JsonProperty
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Future(message = "End time must be in the future")
     LocalDateTime endTime
 ) {
 
