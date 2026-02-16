d986c15f119b204a200df79859f26116126fa06f
Cedric <cedric.noel.2@ulaval.ca>
2026-02-16 15:39:57 -0500

    Added Spring Security

--- src/main/java/com/api/twitchbets/Configuration.java
+++ src/main/java/com/api/twitchbets/Configuration.java
@@ -3,6 +3,8 @@ package com.api.twitchbets;
 import java.time.Clock;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,5 +25,17 @@ public class Configuration {
     @Bean
     public Clock clock() {
         return Clock.systemUTC();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            //todo: remove this in production
+            .csrf().disable()
+            .authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/**").permitAll()
+            );
+
+        return http.build();
     }
 }
